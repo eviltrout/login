96ea107cc79ddb27d649aa9c8b8990bbd0ce04b6
Robin Ward <robin.ward@gmail.com>
2014-08-25 23:41:26 -0400

    Code after screencast

--- tests/integration/login-test.js
+++ tests/integration/login-test.js
@@ -2,13 +2,35 @@ import Ember from 'ember';
 import startApp from '../helpers/start-app';
 
 var App;
+var server;
+
+function parsePostData(query) {
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
 
 module('Integration - Login', {
   setup: function() {
     App = startApp();
+    server = new Pretender(function() {
+      this.post("/session", function(request) {
+        var data = parsePostData(request.requestBody);
+
+        if (data.password === "secret") {
+          return [200, {"Content-Type": "application/json"}, JSON.stringify({success: true})];
+        } else {
+          return [200, {"Content-Type": "application/json"}, JSON.stringify({failure: true})];
+        }
+      });
+    });
   },
   teardown: function() {
     Ember.run(App, 'destroy');
+    server.shutdown();
   }
 });
 
@@ -20,5 +42,16 @@ test("It can login successfully", function() {
 
   andThen(function() {
     equal(find('.alert-success').length, 1, "it shows the success messsage");
+  });
+});
+
+test("It can fail", function() {
+  visit("/");
+  fillIn(".test-username", "Evil Trout");
+  fillIn(".test-password", "wrong password");
+  click(".form-signin button");
+
+  andThen(function() {
+    equal(find('.alert-info').length, 1, "it shows the failure messsage");
   });
 });
